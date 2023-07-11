6f1517c24689c224f97bc8227e7b7b198d9e2560
Abdel B <ab.bailoul@gmail.com>
2023-07-11 07:31:50 +0200

    feat: add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material-module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
@@ -51,6 +53,7 @@ import { ProductsService } from './product/products.service';
     MatRadioModule,
     MatCardModule,
     ReactiveFormsModule,
+    MaterialModule,
   ],
   providers: [ProductsService],
   bootstrap: [AppComponent],
